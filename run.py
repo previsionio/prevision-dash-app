5fb0b3e9b2430858b48dfa23250e67dea5af0d42
Arnold Zephir <arnold.zephir@prevision.io>
2022-01-19 16:35:29 +0100

    interactive chart

--- run.py
+++ run.py
@@ -4,6 +4,7 @@ import plotly.express as px
 
 from dash import dcc
 from dash import html
+from dash.dependencies import Input, Output, State
 
 from services import model 
 
@@ -29,13 +30,23 @@ dashboard.title = 'Analytics Dashboard'
 
 fig = px.bar(df, x="Candidates", y="Similarity", barmode="group")
 
-panelling = html.Div([html.Main(children= "form"), html.Aside(children=    dcc.Graph(
+form = html.Div([
+    html.H3("Texte prononcé"),
+    html.Div([
+        "Texte: ",
+        dcc.Textarea(id='my-input', value="J'aime la France")
+    ]),
+    html.Button('Submit', id='submit-val', n_clicks=0)    
+
+])
+
+panelling = html.Div([html.Main(children= [form]), html.Aside(children=    dcc.Graph(
         id='barchart',
         figure=fig
     ),)], className='vertical-panelling')
-content = html.Div([html.Header, panelling, html.Footer], id="container")
 
-layout = html.Div([dcc.Location(id="url"), panelling, content]) 
+
+
 
 dashboard.layout = html.Div(children=[
     html.Header(children=html.H1(children = 'Prevision.io')),
@@ -43,4 +54,26 @@ dashboard.layout = html.Div(children=[
     html.Footer(children="Copyright")
 ], id="container")
 
+def init_callbacks(dash_app):
+    @dashboard.callback(
+    Output('barchart', 'figure'),
+    Input('submit-val', 'n_clicks'),
+    State('my-input', 'value')    
+    )
+    def update_graph(n_clicks, value):
+        res = model.predict_query(value)
+        df = pd.DataFrame({
+            "Candidates": [candidate["name"] for candidate in res["predictions"]],
+            "Similarity": [candidate["similarity"] for candidate in res["predictions"]],
+
+        })
+        fig = px.bar(df, x="Candidates", y="Similarity")
+        fig.update_layout(transition_duration=500)
+        return fig
+
+
+init_callbacks(dashboard)
 app = dashboard.server
+
+
+
